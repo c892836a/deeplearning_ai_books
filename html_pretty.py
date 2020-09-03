1b2261eadbcf9ec25993027ab1b2e5acc8d6d540
rei64527 <rei64527@gmail.com>
2020-09-03 16:23:56 +0800

    add dark mode

--- html_pretty.py
+++ html_pretty.py
@@ -1,6 +1,15 @@
+import os
+from os import walk
 from bs4 import BeautifulSoup
+f = []
+for (dirpath, dirnames, filenames) in walk('html'):
+    f.extend(filenames)
+    break
 
-file = open("html\lesson2-week1_new.html", "w", encoding="utf-8")
-soup = BeautifulSoup(open("html\lesson2-week1.html", encoding='utf-8'), features="html.parser")
-file.write(soup.prettify())
-file.close()
+for filename in f:
+    if ".html" not in filename:
+        continue
+    soup = BeautifulSoup(open(os.path.join("html", filename), encoding="utf-8"), features="html.parser")
+    file = open(os.path.join("html", filename), "w", encoding="utf-8")
+    file.write(soup.prettify())
+    file.close()
